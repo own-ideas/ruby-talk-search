870b89adcbadda6b5dc0e45ca177b15a75f856c3
Mikhail Krainik <mikhail.krainik@gmail.com>
2017-07-22 20:17:41 +0300

    initialize commit

---
+++ lib/ruby_talk_search.ex
@@ -0,0 +1,18 @@
+defmodule RubyTalkSearch do
+  @moduledoc """
+  Documentation for RubyTalkSearch.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RubyTalkSearch.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
